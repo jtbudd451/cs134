e25f14f56a5789ec1e6185067a72af49714c096a
jtbudd451 <buddwebsolutions@gmail.com>
2017-06-14 15:50:00 -0400

    Lab 10
    intro js exercise

---
+++ lab10/script.js
@@ -0,0 +1,6 @@
+var userName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+if(userName == "john" && lastName == "doe") {
+	alert("You win a million dollars " + userName + " "+ lastName);
+	console.log("Also great to meet you, " + userName);
+}
